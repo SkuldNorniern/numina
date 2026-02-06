3ce44f04b1fa27554b9b0d3c52fa8c98c367d41c
Eira <skuldnorniern@gmail.com>
2026-02-06 16:30:41 +0900

    feat: expose new dtypes

--- src/dtype/types/mod.rs
+++ src/dtype/types/mod.rs
@@ -2,8 +2,16 @@
 
 // Module declarations
 pub mod bfloat16;
+pub mod bfloat8;
+pub mod complex;
+pub mod float16;
+pub mod float8;
 pub mod quantized;
 
 // Re-exports for convenience
 pub use bfloat16::BFloat16;
+pub use bfloat8::BFloat8;
+pub use complex::{Complex128, Complex32, Complex64};
+pub use float16::Float16;
+pub use float8::{Float8E4M3Fn, Float8E5M2};
 pub use quantized::*;
